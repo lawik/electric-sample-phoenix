ef6f93493d0dc63097b9d577a59fda8a0f430471
Lars Wikman <lars@underjord.io>
2023-02-24 13:19:22 +0100

    move to other port

--- config/dev.exs
+++ config/dev.exs
@@ -22,7 +22,7 @@ config :admin, Admin.Repo,
 config :admin, AdminWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {0, 0, 0, 0}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4003],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
